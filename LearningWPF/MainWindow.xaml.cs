5d320d81f1a61662862b0ac1dca7e66b8b520377
BionicHawk <angel.manzo.rosas@gmail.com>
2024-03-19 17:50:50 -0600

    Agregar archivos de proyecto.

---
+++ LearningWPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace LearningWPF
+{
+    public partial class MainWindow : Window
+    {
+        private int count = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbHello.Text = $"Hello world {count}";
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            tbHello.Text = $"Hello world {++count}";
+        }
+    }
+}
